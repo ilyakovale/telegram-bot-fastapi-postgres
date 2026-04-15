0b8f088a82c22c7befc4bf43d78c0c2af54ace1e
ilya kovalenko <inter_kovalenko_ilya@mail.ru>
2026-04-15 17:23:07 +0300

    add admin_panel

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import asyncio
 from dotenv import load_dotenv, dotenv_values
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 import os
@@ -18,8 +18,6 @@ if not TOKEN:
     exit(1)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я бот!")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Команды: /start, /help")
@@ -39,7 +37,47 @@ async def admin_check(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("У вас нет прав администратора! ❌")
         return False
+
+async def admin_panel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if await admin_check(update, context):
+        keyboard = [
+            [KeyboardButton("📊 Статистика"), KeyboardButton("⚙️ Настройки")],
+            [KeyboardButton("🔒 Управление пользователями")],
+            [KeyboardButton("📦 Управление заказами")]
+        ]
+        
+        reply_markup1 = ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True,
+            one_time_keyboard=False
+        )
+        
+        await update.message.reply_text(
+            "Панель администратора:",
+            reply_markup=reply_markup1
+        )
     
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Создаём кнопки
+    keyboard = [
+        [KeyboardButton("📦 Заказать")],
+        [KeyboardButton("ℹ️ Аккаунт"), KeyboardButton("📞 Контакты")],
+        [KeyboardButton("📍 Адрес"), KeyboardButton("🕐 Режим работы")],
+    ]
+    
+    reply_markup = ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True,  # Автоматический размер
+        one_time_keyboard=False  # Не скрывать после нажатия
+    )
+    
+    await update.message.reply_text(
+        "Выберите пункт меню:",
+        reply_markup=reply_markup
+    )
+    
+
+
 def main():
     # Создаем приложение
     app = Application.builder().token(TOKEN).build()
@@ -48,7 +86,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("stop", stop_command))
-    
+    app.add_handler(CommandHandler("admin_panel", admin_panel))
     print("🤖 Бот запущен...")
     app.run_polling(allowed_updates=[])
 
