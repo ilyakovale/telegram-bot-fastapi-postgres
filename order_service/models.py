1c033d27eee4848d58c34ca8e77f01158363bb0a
ilya kovalenko <inter_kovalenko_ilya@mail.ru>
2026-08-12 14:42:06 +0300

    delete order_service utils

--- order_service/models.py
+++ order_service/models.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, Date
+from sqlalchemy import Column, Date, BigInteger, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
-from database import Base, engine
+from database import Base
+
 
 class Order(Base):
     __tablename__ = "orders"
 
-    date = Column(Date, primary_key=True)
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    chat_id = Column(BigInteger, ForeignKey("accounts.chat_id"), nullable=False)
+    date = Column(Date, nullable=False)
     last_date_before_registration = Column(Date, nullable=False)
     products_max = Column(JSONB, nullable=False, default=list)
     products_current = Column(JSONB, nullable=False, default=list)
-
-
-
-    
